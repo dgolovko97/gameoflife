18e78e347811210775aec9dea84d279746bcc5e3
d.golovko <dmitrigolovko13@gmail.com>
2023-03-30 20:20:38 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const container: HTMLElement | null = document.getElementById("root");
+const root = ReactDOM.createRoot(container!);
+root.render(<div>123</div>);
